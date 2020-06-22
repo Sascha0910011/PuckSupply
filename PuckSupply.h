dac66b096a0550c1b31f2b2aed8787f2ca2647ec
Eizaveta_2000 <elizavetapotapova24@gmail.com>
2020-06-22 19:08:21 +0300

    p2

---
+++ PuckSupply.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include "Puck.h"
+class PuckSupply
+{
+public:
+	PuckSupply(int HP);
+	Puck getPuck();
+	~PuckSupply();
+	friend class GameTable;
+	friend void GameRun();
+
+	
+};
+
